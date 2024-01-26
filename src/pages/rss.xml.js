4b38f4555aac30db12278af99b1f9bfb00d97f38
Gabriel Palhares <gabrielpalharesdev@gmail.com>
2024-01-26 10:31:30 -0300

    I think I fixed build

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,12 +1,10 @@
 import rss from "@astrojs/rss";
 
-const postImportResult = import.meta.globEager("./posts/*.md");
-const posts = Object.values(postImportResult);
-
-export const get = () =>
-  rss({
-    title: "Astro Theme Creek",
-    description: "A Theme for Astro",
+export function GET(context) {
+  return rss({
+    title: "Podbits",
+    description: "Tech Talsk, Bit-ready for you",
     site: import.meta.env.SITE,
     items: import.meta.glob("./posts/**/*.md"),
   });
+}
