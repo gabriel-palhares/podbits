ef243bd4955c8e96d9c52a951df1754781994ec0
Gabriel <gabrielpalharesdev@gmail.com>
2024-07-02 15:28:33 -0300

    add reading time feature to blogposts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
 import { defineConfig } from "astro/config";
+import { remarkReadingTime } from './remark-reading-time.mjs';
 
 export default defineConfig({
   site: "https://gabrielpalhares.dev/podbits/",
+  markdown: {
+    remarkPlugins: [remarkReadingTime],
+  },
 });
---
+++ remark-reading-time.mjs
@@ -0,0 +1,12 @@
+import getReadingTime from 'reading-time';
+import { toString } from 'mdast-util-to-string';
+
+export function remarkReadingTime() {
+  return function (tree, { data }) {
+    const textPage = toString(tree);
+    const readingTime = getReadingTime(textoPagina);
+    // tempoLeitura.text nos dará minutos lidos como uma string amigável
+    // i.e. "3 min read"
+    data.astro.frontmatter.minutesRead = readingTime.text;
+  };
+}
