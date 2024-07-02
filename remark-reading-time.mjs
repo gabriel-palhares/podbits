43aa20b5bcee243efa4685268d7345e0c4ab30e2
Gabriel <gabrielpalharesdev@gmail.com>
2024-07-02 15:30:50 -0300

    fix remark reading time plugin

--- remark-reading-time.mjs
+++ remark-reading-time.mjs
@@ -4,7 +4,7 @@ import { toString } from 'mdast-util-to-string';
 export function remarkReadingTime() {
   return function (tree, { data }) {
     const textPage = toString(tree);
-    const readingTime = getReadingTime(textoPagina);
+    const readingTime = getReadingTime(textPage);
     // tempoLeitura.text nos dará minutos lidos como uma string amigável
     // i.e. "3 min read"
     data.astro.frontmatter.minutesRead = readingTime.text;
